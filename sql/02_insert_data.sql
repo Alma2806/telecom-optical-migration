adc0b7553858cb8f47bbfcb666b13193d8ab6af3
Alma2806 <136968117+Alma2806@users.noreply.github.com>
2025-04-18 11:57:43 +0200

    02_insert_data.sql

--- sql/02_insert_data.sql
+++ sql/02_insert_data.sql
@@ -1,7 +1,15 @@
-SELECT operator_name, country,
-       SUM(CASE WHEN technology = 'copper' THEN total_users_thousands ELSE 0 END) AS users_before_optical,
-       SUM(CASE WHEN technology = 'optical' THEN total_users_thousands ELSE 0 END) AS users_on_optical
-FROM telecom_data
-GROUP BY operator_name, country;
--- SQL to insert data
-INSERT INTO telecom_data VALUES (...);
+INSERT INTO telecom_data VALUES
+('Deutsche Telekom', 'Germany', 2018, 'copper', 10000, 0, 500, 3000, 0),
+('Deutsche Telekom', 'Germany', 2022, 'optical', 6000, 3000, 1500, 5000, 1800),
+('Deutsche Telekom', 'Germany', 2025, 'optical', 8500, 2500, 1800, 7000, 3000);
+
+INSERT INTO telecom_data VALUES
+('Orange', 'France', 2018, 'copper', 9500, 0, 450, 2800, 0),
+('Orange', 'France', 2022, 'optical', 7000, 4000, 1600, 5200, 2200),
+('Orange', 'France', 2025, 'optical', 9500, 2500, 2000, 7600, 3200);
+
+INSERT INTO telecom_data VALUES
+('Telefónica', 'Spain', 2018, 'copper', 8700, 0, 400, 2600, 0),
+('Telefónica', 'Spain', 2022, 'optical', 8000, 3500, 1400, 4900, 2000),
+('Telefónica', 'Spain', 2025, 'optical', 9800, 1800, 1700, 6900, 3100);
+
