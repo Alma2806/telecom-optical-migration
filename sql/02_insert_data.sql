e73df202e231f156ca6114cdd01e10dcf970a134
Alma2806 <136968117+Alma2806@users.noreply.github.com>
2025-04-18 11:52:10 +0200

    02_insert_data.sql

--- sql/02_insert_data.sql
+++ sql/02_insert_data.sql
@@ -1,2 +1,7 @@
+SELECT operator_name, country,
+       SUM(CASE WHEN technology = 'copper' THEN total_users_thousands ELSE 0 END) AS users_before_optical,
+       SUM(CASE WHEN technology = 'optical' THEN total_users_thousands ELSE 0 END) AS users_on_optical
+FROM telecom_data
+GROUP BY operator_name, country;
 -- SQL to insert data
 INSERT INTO telecom_data VALUES (...);
